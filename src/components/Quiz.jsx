de943d10cc934fe692142612ede0ec3ce3af2bbd
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-05-07 11:03:52 +0100

    Added a quiz completed screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import QUESTIONS from "../content/questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
   function handleClick(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+  if (quizIsOver) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Complete</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
